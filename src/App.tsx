bba531b315ba1b4f5d8a693dfbd3cc86e2ab4ed7
ria830617 <ria830617.gmail.com>
2022-12-06 13:48:56 +0900

    typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+function App() {
+  return (
+    <Wrapper>
+      <Title>타이틀</Title>
+    </Wrapper>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,10 @@ const lightTheme = {
   backgroundColor: "whitesmoke",
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={darkTheme}>
